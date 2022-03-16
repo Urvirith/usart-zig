c21edddb6638ba1c95e3988bc8c35e301cf64e24
Jake Mussler <jake.mussler@protonmail.com>
2022-03-16 14:31:03 +0000

    Reflective message complete.

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const usart = @import("usart.zig");
 
 pub fn main() anyerror!u8 {
     const port_name = "/dev/ttyUSB0";
+    var buffer: [1024]u8 = undefined;
 
     var serial = std.fs.cwd().openFile(port_name, .{ .read = true, .write = true }) catch |err| switch (err) {
         error.FileNotFound => {
@@ -29,6 +30,9 @@ pub fn main() anyerror!u8 {
         try serial.writer().writeAll("Hello, World!\r\n");
 
         std.time.sleep(1000000000);
+        var dog = try serial.reader().readUntilDelimiter(&buffer, '\n');
+
+        try std.io.getStdOut().writer().print("Data: {s}\n", .{dog});
         i+=1;
     }
 
