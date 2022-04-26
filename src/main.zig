ad5c0723e87e9323db079b42e830e863b31740c4
Jake Mussler <jake.mussler@protonmail.com>
2022-04-26 18:53:42 +0100

    Updated for STM32 diagnosis

--- src/main.zig
+++ src/main.zig
@@ -15,25 +15,25 @@ pub fn main() anyerror!u8 {
     defer serial.close();
 
     var config = usart.SerialPort.init(
-        usart.BaudRate.baud9600, 
+        usart.BaudRate.baud921600, 
         usart.Parity.none, 
         usart.StopBits.one, 
-        usart.WordLength.seven,
+        usart.WordLength.eight,
         usart.FlowControl.none
     );
 
     try config.configure(serial);
 
-    var i: u32 = 0;
+    //var i: u32 = 0;
     
-    while (i < 10) {
-        try serial.writer().writeAll("Hello, World!\r\n");
+    while (true) {
+        //try serial.writer().writeAll("Hello, World!\r\n");
 
-        std.time.sleep(1000000000);
-        var dog = try serial.reader().readUntilDelimiter(&buffer, '\n');
+        //std.time.sleep(1000000000);
+        var dog = try serial.reader().readUntilDelimiter(&buffer, 0x0D);
 
-        try std.io.getStdOut().writer().print("Data: {s}\n", .{dog});
-        i+=1;
+        try std.io.getStdOut().writer().print("Data: {d}\n", .{dog});
+        //i+=1;
     }
 
     return 0;
