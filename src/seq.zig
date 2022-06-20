c51d5cb9cdb3228214e1da7f5ab20cc812f14d65
Jake Mussler <jake.mussler@protonmail.com>
2022-06-20 15:00:18 +0100

    Added new file, description of sequence

---
+++ src/seq.zig
@@ -0,0 +1,7 @@
+// Main Sequence for Zig,
+// 1) Read Scans
+// 2) Save Scans
+// 3) Send Type, Part Info, SO to Printer
+// 4) Send SO to other printer in correct format
+// 5) Send SO to server via JSON
+// 6) Send Confirmation message
