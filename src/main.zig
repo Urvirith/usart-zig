99a7436dff3672eba78739966fba26e879df9360
Jake Mussler <jake.mussler@protonmail.com>
2022-06-20 14:38:25 +0100

    Updated and tested the USART, using loop.  Works

--- src/main.zig
+++ src/main.zig
@@ -1,42 +1,37 @@
 const std = @import("std");
 const usart = @import("usart.zig");
 
-pub fn main() anyerror!u8 {
+pub fn main() anyerror!u8 {    
     const port_name = "/dev/ttyUSB0";
     var buffer: [1024]u8 = undefined;
 
-    var serial = std.fs.cwd().openFile(port_name, .{ .read = true, .write = true }) catch |err| switch (err) {
+    var dev = std.fs.cwd().openFile(port_name, .{ .read = true, .write = true }) catch |err| switch (err) {
         error.FileNotFound => {
             try std.io.getStdOut().writer().print("The serial port {s} does not exist.\n", .{port_name});
             return 1;
         },
         else => return err,
     };
-    defer serial.close();
+    defer dev.close();
 
     var config = usart.SerialPort.init(
-        usart.BaudRate.baud921600, 
+        usart.BaudRate.baud4800, 
         usart.Parity.none, 
         usart.StopBits.one, 
         usart.WordLength.eight,
         usart.FlowControl.none
     );
 
-    try config.configure(serial);
-
-    //var i: u32 = 0;
+    try config.configure(dev);
     
     while (true) {
-        //try serial.writer().writeAll("Hello, World!\r\n");
+        try dev.writer().writeAll("Hello, World!\n");
 
-        //std.time.sleep(1000000000);
-        var dog = try serial.reader().readUntilDelimiter(&buffer, 0xFF);
+        std.time.sleep(1000000000);
+        var len = try dev.reader().readUntilDelimiter(&buffer, '\n');
 
-        try std.io.getStdOut().writer().print("Data: {d}\n", .{dog});
-        //i+=1;
+        try std.io.getStdOut().writer().print("Data: {s}\n", .{len});
     }
 
     return 0;
 }
-
-
